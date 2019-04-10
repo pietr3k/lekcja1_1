5e87aea665125d1af4f63597371b960333e4a008
grygop <roman.pierdzistolek@gmail.com>
2019-04-10 15:25:19 +0200

    mała zmiana

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean warun = true;
+        char znak = 'a';
+        int liczba = 12;
+        long liczba1 = 1234;
+        float liczba2 = 1234.2f;
+        double liczba3 = 12345.12;
+        String tekst = "nauka programowania, pzodrawiam";
+        System.out.println(warun);
+        System.out.println(znak);
+        System.out.println(liczba);
+        System.out.println(liczba);
+        System.out.println(liczba1);
+        System.out.println(liczba2);
+        System.out.println(liczba3);
+        System.out.println(tekst);
+
+
+    }
+}
